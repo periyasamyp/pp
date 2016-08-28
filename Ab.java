ae69b4dd39487e14984acc4f34c4351cbe280681
periyasamyp <periyasamy753@gmail.com>
2016-08-28 11:28:24 +0530

    Create Ab.java

---
+++ Ab.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Ab
+{
+public static void main(string args[])
+{
+Scanner sb=new Scanner(System.in);
+string n;
+n=sb.nextLine();
+if(n=="sunday")
+{
+System.out.println("false");
+}
+else
+{
+System.out.println("true");
+}
+}
+}
+
